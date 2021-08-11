b7f8e12bf03ed49fd7008f55a8154d61261bdbd8
byLLIPyT <byllipyt@icloud.com>
2021-08-11 11:08:31 +0300

    +initial commit

---
+++ CurrencyListTest/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CurrencyListTest
+//
+//  Created by Александр Уткин on 11.08.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
