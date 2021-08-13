25826a21eba6415f86116585c2554b9e17aca3a1
byLLIPyT <byllipyt@icloud.com>
2021-08-13 22:14:13 +0300

    add refresh control

--- CurrencyListTest/CurrencyList/MainViewController.swift
+++ CurrencyListTest/CurrencyList/MainViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
+    private var refControl = UIRefreshControl()
     var USDCourse: [USD] = []
     var value = String()
     var recordDate = String()
@@ -17,36 +18,17 @@ class MainViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let month = Month()
         let fromDate = month.dateMonthAgo()
         let toDate = month.startMonth()
-        
         tableView.register(CurrencyCell.self, forCellReuseIdentifier: "Cell")
         configureLimitButton()
         networkManager.fetchXML(delegate: self, fromDate: fromDate, toDate: toDate, currencyCode: "R01235")
-    }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        configureRefreshControl()
         
-        return USDCourse.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CurrencyCell
-        cell.configureCell(currency: USDCourse[indexPath.row])
-        return cell
-    }
-        
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
     }
     
     private func configureLimitButton() {
-        
         let limitButton = UIButton(type: .custom)
         limitButton.setTitle("Установить лимит", for: .normal)
         limitButton.setTitleColor(.blue, for: .normal)
@@ -57,5 +39,44 @@ class MainViewController: UITableViewController {
 
     @objc func limitPrice() {
         showAlert()
+    }
+    
+    private func configureRefreshControl() {
+        refControl.attributedTitle = NSAttributedString(string: "")
+        refControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableView.addSubview(refControl)
+    }
+
+    @objc func refresh() {
+        let month = Month()
+        let fromDate = month.dateMonthAgo()
+        let toDate = month.startMonth()
+        DispatchQueue.main.async {
+            self.networkManager.fetchXML(delegate: self, fromDate: fromDate, toDate: toDate, currencyCode: "R01235")
+            self.tableView.reloadData()
+        }
+        refControl.endRefreshing()
+    }
+    
+// MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return USDCourse.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CurrencyCell
+        cell.configureCell(currency: USDCourse[indexPath.row])
+        return cell
+    }
+
+// MARK: - UITableViewDelegate
+   
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
