9ed56d3b7df22196474acbefe6264b0b8aad77d1
byLLIPyT <byllipyt@icloud.com>
2021-08-15 07:48:37 +0300

    +add spinner

--- CurrencyListTest/CurrencyList/MainViewController.swift
+++ CurrencyListTest/CurrencyList/MainViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class MainViewController: UITableViewController {
-
+    
+    let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
     private var refControl = UIRefreshControl()
     var USDCourse: [Currency] = []
     var value = String()
@@ -18,15 +25,21 @@ class MainViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        tableView.addSubview(spinner)
         let month = Month()
         let fromDate = month.dateMonthAgo()
         let toDate = month.startMonth()
         tableView.register(CurrencyCell.self, forCellReuseIdentifier: "Cell")
         tableView.separatorStyle = .none
         configureLimitButton()
-        networkManager.fetchXML(delegate: self, fromDate: fromDate, toDate: toDate, currencyCode: "R01235") {
-            print("")
-        }
+        NSLayoutConstraint.activate([
+            spinner.centerYAnchor.constraint(equalTo: tableView.centerYAnchor),
+            spinner.centerXAnchor.constraint(equalTo: tableView.centerXAnchor)
+        ])
+        spinner.startAnimating()
+        networkManager.fetchXML(delegate: self, fromDate: fromDate, toDate: toDate, currencyCode: "R01235") { }
+        self.spinner.stopAnimating()
         configureRefreshControl()
         checkLimitPrice()
     }
@@ -39,7 +52,7 @@ class MainViewController: UITableViewController {
         let item = UIBarButtonItem(customView: limitButton)
         self.navigationItem.setRightBarButton(item, animated: true)
     }
-
+        
     @objc func limitPrice() {
         showAlert()
     }
@@ -49,7 +62,7 @@ class MainViewController: UITableViewController {
         refControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
         tableView.addSubview(refControl)
     }
-
+    
     @objc func refresh() {
         let month = Month()
         let fromDate = month.dateMonthAgo()
@@ -73,7 +86,7 @@ class MainViewController: UITableViewController {
         }
     }
     
-// MARK: - UITableViewDataSource
+    // MARK: - UITableViewDataSource
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return USDCourse.count
@@ -84,9 +97,9 @@ class MainViewController: UITableViewController {
         cell.configureCell(currency: USDCourse[indexPath.row])
         return cell
     }
-
-// MARK: - UITableViewDelegate
-   
+    
+    // MARK: - UITableViewDelegate
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
--- CurrencyListTest/Services/NetworkManager.swift
+++ CurrencyListTest/Services/NetworkManager.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct NetworkManager {
     
-    func fetchXML(delegate: MainViewController, fromDate: String, toDate: String, currencyCode: String, completion: () -> Void) {
+    func fetchXML(delegate: MainViewController, fromDate: String, toDate: String, currencyCode: String, completion: (() -> Void)?) {
         let urlString = "http://cbr.ru/scripts/XML_dynamic.asp?date_req1=\(fromDate)&date_req2=\(toDate)&VAL_NM_RQ=\(currencyCode)"
         
         guard let url = URL(string: urlString) else { return }
         if let parser = XMLParser(contentsOf: url) {
             parser.delegate = delegate
-            completion()
+            completion?()
             if !parser.parse() {
                 delegate.showMessageAlert(title: "Error", message: "Don't fetch XML")
             } 
